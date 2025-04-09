e5d7d679eb9d59510ca32036c3862d54b6f72ccb
ferreiraluc <l.adrianobf@gmail.com>
2025-04-09 18:50:55 -0300

    implementando JPA

--- src/main/java/zad_inventory/entity/OperacaoEntity.java
+++ src/main/java/zad_inventory/entity/OperacaoEntity.java
@@ -1,32 +1,67 @@
 package zad_inventory.entity;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "operacao")
 public class OperacaoEntity {
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "fk_produto_id")
     private int produtoId;
+
+    @Column(name = "produto_nome")
     private String produtoNome;
+
+    @Column(name = "fk_usuario_id")
     private int usuarioId;
+
+    @Column(name = "usuario_nome")
     private String usuarioNome;
-    private int situacaoId;
+
+    @Column(name = "fk_situacao")
+    private String situacao;
+
+    @Column(name = "quantidade")
     private int quantidade;
+
+    @Column(name = "data")
     private LocalDateTime data;
 
     public OperacaoEntity() {}
 
-
-    public OperacaoEntity(int id, int produtoId, int usuarioId, int situacaoId, int quantidade, LocalDateTime data) {
+    public OperacaoEntity(Long id, int produtoId, String produtoNome, int usuarioId, String usuarioNome, String situacao, int quantidade, LocalDateTime data) {
         this.id = id;
         this.produtoId = produtoId;
+        this.produtoNome = produtoNome;
         this.usuarioId = usuarioId;
-        this.situacaoId = situacaoId;
+        this.usuarioNome = usuarioNome;
+        this.situacao = situacao;
         this.quantidade = quantidade;
         this.data = data;
     }
 
+    @Override
+    public String toString() {
+        return "OperacaoEntity{" +
+                "id=" + id +
+                ", produtoId=" + produtoId +
+                ", produtoNome='" + produtoNome + '\'' +
+                ", usuarioId=" + usuarioId +
+                ", usuarioNome='" + usuarioNome + '\'' +
+                ", situacao='" + situacao + '\'' +
+                ", quantidade=" + quantidade +
+                ", data=" + data +
+                '}';
+    }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    // Getters e Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
     public int getProdutoId() { return produtoId; }
     public void setProdutoId(int produtoId) { this.produtoId = produtoId; }
@@ -40,24 +75,12 @@ public class OperacaoEntity {
     public String getUsuarioNome() { return usuarioNome; }
     public void setUsuarioNome(String usuarioNome) { this.usuarioNome = usuarioNome; }
 
-    public int getSituacaoId() { return situacaoId; }
-    public void setSituacaoId(int situacaoId) { this.situacaoId = situacaoId; }
+    public String getSituacaoNome() { return situacao; }
+    public void setSituacaoNome(String situacaoNome) { this.situacao = situacaoNome; }
 
     public int getQuantidade() { return quantidade; }
     public void setQuantidade(int quantidade) { this.quantidade = quantidade; }
 
     public LocalDateTime getData() { return data; }
     public void setData(LocalDateTime data) { this.data = data; }
-
-    @Override
-    public String toString() {
-        return "Operacao{" +
-                "id=" + id +
-                ", produto=" + produtoNome +
-                ", usuarioId=" + usuarioNome +
-                ", situacaoId=" + situacaoId +
-                ", quantidade=" + quantidade +
-                ", data=" + data +
-                '}';
-    }
 }
