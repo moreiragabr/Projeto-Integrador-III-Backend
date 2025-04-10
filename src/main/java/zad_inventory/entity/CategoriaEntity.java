459341f41c152d17967e96d9052abae91c023e7f
Mateus <mateusmelocantero@gmail.com>
2025-04-09 22:19:19 -0300

    Update CategoriaEntity.java

--- src/main/java/zad_inventory/entity/CategoriaEntity.java
+++ src/main/java/zad_inventory/entity/CategoriaEntity.java
@@ -1,4 +1,66 @@
 package zad_inventory.entity;
 
-public class CategoriaEntity {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, unique = true)
+    private String nome;
+    
+    @Column
+    private String descricao;
+
+    public Categoria() {
+    }
+
+  
+    public Categoria(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+   
+    @Override
+    public String toString() {
+        return "Categoria [id=" + id + ", nome=" + nome + ", descricao=" + descricao + "]";
+    }
+    
+    public static class CategoriaHelper {
+        public static String formatarNome(Categoria categoria) {
+            return categoria.getNome().toUpperCase();
+        }
+    }
 }
