31983f6f6f06f8b3f371018151289cab1384c286
dev-cantero <mateusmelocantero@gmail.com>
2025-04-10 20:17:49 -0300

    Erros da CategoriaEntity arrumados e CategoriaRepository feito

--- src/main/java/zad_inventory/entity/CategoriaEntity.java
+++ src/main/java/zad_inventory/entity/CategoriaEntity.java
@@ -1,6 +1,5 @@
 package zad_inventory.entity;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -20,7 +19,7 @@ public class CategoriaEntity{
     @Column
     private String descricao;
 
-    public Categoria() {
+    public CategoriaEntity() {
     }
 
   
--- src/main/java/zad_inventory/repository/CategoriaRepository.java
+++ src/main/java/zad_inventory/repository/CategoriaRepository.java
@@ -1,4 +1,39 @@
 package zad_inventory.repository;
 
+import zad_inventory.entity.CategoriaEntity;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class CategoriaRepository {
+
+    private EntityManager em;
+
+    //CRUD
+
+    public void salvar(CategoriaRepository categoria){
+        em.getTransaction().begin();
+        em.persist(categoria);
+        em.getTransaction().commit();
+    }
+
+    public List<CategoriaRepository> buscarTodos(){
+        return em.createQuery("SELECT s FROM situacao s", CategoriaRepository.class).getResultList();
+    }
+
+    public CategoriaRepository buscarPorId(Long id){
+        return em.find(CategoriaRepository.class, id);
+    }
+
+    public void atualizar(CategoriaRepository categoria){
+        em.getTransaction().begin();
+        em.merge(categoria);
+        em.getTransaction().commit();
+    }
+
+    public void remover(CategoriaRepository categoria){
+        em.getTransaction().begin();
+        em.remove(em.contains(categoria) ? categoria : em.merge(categoria));
+        em.getTransaction().commit();
+    }
 }
