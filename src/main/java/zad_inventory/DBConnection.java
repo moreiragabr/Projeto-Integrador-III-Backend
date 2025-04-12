432da154da474656837b947bf946e3066fb67d1d
Pedro-Maldonado <pedrolmald@gmail.com>
2025-04-11 21:15:45 -0300

    Update DBConnection.java
    Teste realizado

--- src/main/java/zad_inventory/DBConnection.java
+++ src/main/java/zad_inventory/DBConnection.java
@@ -1,4 +1,20 @@
-package zad_inventory;
+package zad_inventory.config;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class DBConnection {
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("zad_inventory");
+
+    public static EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public static void close() {
+        if (emf != null && emf.isOpen()) {
+            emf.close();
+        }
+    }
 }
